3d08a31417521b21063a792ca57a60a7ae8b5811
kreaper97 <eric97lopez@gmail.com>
2025-02-05 17:44:50 +0100

    fix precio spcript

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS PRICE (
     price_list INT NOT NULL,
     product_id BIGINT NOT NULL,
     priority INT NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
     currency VARCHAR(3) NOT NULL,
     CONSTRAINT fk_brand FOREIGN KEY (brand_id) REFERENCES BRAND(id),
     CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES PRODUCT(id)
@@ -33,7 +33,7 @@ INSERT INTO PRODUCT (id, name) VALUES (98765, 'Zapatos Deportivos');
 INSERT INTO PRODUCT (id, name) VALUES (54321, 'Bolso de Cuero');
 
 -- Insertar precios asociados a ZARA
-INSERT INTO PRICE (id, brand_id, product_id, start_date, end_date, price_list, priority, price, currency) VALUES
+INSERT INTO PRICE (id, brand_id, product_id, start_date, end_date, price_list, priority, precio, currency) VALUES
 (1, 1, 35455, '2020-06-14 00:00:00', '2020-12-31 23:59:59', 1, 0, 35.50, 'EUR'),
 (2, 1, 35455, '2020-06-14 15:00:00', '2020-06-14 18:30:00', 2, 1, 25.45, 'EUR'),
 (3, 1, 35455, '2020-06-15 00:00:00', '2020-06-15 11:00:00', 3, 1, 30.50, 'EUR'),
